6ebfba7b9bbb6d8be2c80d04a52f67b39efc03d2
Kacper <kacper.krol@itsquad.pl>
2025-05-10 12:34:37 +0200

    Add project files.

---
+++ help-form-assistant/Web/ChatController.cs
@@ -0,0 +1,34 @@
+using help_form_assistant.Application;
+using Microsoft.AspNetCore.Mvc;
+
+namespace help_form_assistant.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ChatController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<ChatController> _logger;
+
+        public ChatController(ILogger<ChatController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
